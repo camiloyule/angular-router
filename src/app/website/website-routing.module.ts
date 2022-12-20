1f33eac3539ee4065d29f0a01c0ecfd437fdd489
Camilo Yule <camiloyule@Camilos-MacBook-Pro.local>
2022-12-19 20:55:33 -0500

    preload strategies

--- src/app/website/website-routing.module.ts
+++ src/app/website/website-routing.module.ts
@@ -4,7 +4,6 @@ import { RouterModule, Routes } from '@angular/router';
 import { LayoutComponent } from './components/layout/layout.component';
 import { HomeComponent } from './pages/home/home.component';
 
-import { CategoryComponent } from './pages/category/category.component';
 import { MyCartComponent } from './pages/my-cart/my-cart.component';
 import { LoginComponent } from './pages/login/login.component';
 import { RegisterComponent } from './pages/register/register.component';
@@ -28,8 +27,11 @@ const routes: Routes = [
         component: HomeComponent,
       },
       {
-        path: 'category/:id',
-        component: CategoryComponent,
+        path: 'category',
+        loadChildren: ()=> import ('./pages/category/category.module').then((m)=>m.CategoryModule),
+        data: {
+          preload: true,
+        }
       },
       {
         path: 'product/:id',
--- src/app/website/website.module.ts
+++ src/app/website/website.module.ts
@@ -6,7 +6,7 @@ import { SharedModule } from '../shared/shared.module';
 
 import { NavComponent } from './components/nav/nav.component';
 import { HomeComponent } from './pages/home/home.component';
-import { CategoryComponent } from './pages/category/category.component';
+
 import { MyCartComponent } from './pages/my-cart/my-cart.component';
 import { LoginComponent } from './pages/login/login.component';
 import { RegisterComponent } from './pages/register/register.component';
@@ -22,7 +22,7 @@ import { SwiperModule } from 'swiper/angular';
     NavComponent,
 
     HomeComponent,
-    CategoryComponent,
+
     MyCartComponent,
     LoginComponent,
     RegisterComponent,
